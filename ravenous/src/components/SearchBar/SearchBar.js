7909394ca83eac0912a96e881375b39a3b2489e3
Abubakar Siddique <abz.siddique@gmail.com>
2019-06-24 15:20:06 +0100

    Building structure of searchBar component

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -38,4 +38,24 @@ class SearchBar extends React.Component {
       <li key={sortByOptionValue}>{sortByOption}</li>
     });
   }
+
+  render() {
+    return (
+      <div className="SearchBar">
+      <div className="SearchBar-sort-options">
+        <ul>
+          <!-- Use .renderSortByOptions() to sort the businesses by their options -->
+        </ul>
+      </div>
+      <div className="SearchBar-fields">
+        <input placeholder="Search Businesses" />
+        <input placeholder="Where?" />
+      </div>
+      <div className="SearchBar-submit">
+        <a>Let's Go</a>
+      </div>
+    </div>
+    );
+  }
+
 }
